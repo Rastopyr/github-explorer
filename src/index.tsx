7c673331cbb80d16b3a0f3c502be3ebeff32db5e
Roma Senin <rastopyr@gmail.com>
2022-05-29 18:13:44 +0200

    fix: add github provider and types for DOM var

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
+import { GithubAPIProvider } from "./github-api/GithubAPIProvider";
 
 const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("Root element not provided for React");
+
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <GithubAPIProvider>
+      <App />
+    </GithubAPIProvider>
   </StrictMode>
 );
